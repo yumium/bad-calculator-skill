9e98fe5b5a2d9f996ce25d64200bf007ba28634d
yumium <zaiyuming@gmail.com>
2021-09-06 12:05:01 +0300

    Added stalk methods in __init__.py

--- __init__.py
+++ __init__.py
@@ -5,10 +5,12 @@ class BadCalculator(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('calculator.bad.intent')
+    @intent_handler('calculator.bad.intent')
     def handle_calculator_bad(self, message):
         self.speak_dialog('calculator.bad')
 
+    def stop(self):
+        self.speak_dialog("You can't stop me")
 
 def create_skill():
     return BadCalculator()
